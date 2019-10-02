7431c88664ee19c118640fbac24f1c80a461442a
scalerone <676119236@qq.com>
2019-10-02 19:57:23 +0800

    登录接口中使用jwt实现token返回

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const  bcrypt = require('bcrypt');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/key');
 
 const User = require('../../models/User');
 
@@ -57,5 +59,40 @@ router.post('/register', (req, res) => {
 
 });
 
+
+// @route  POST api/users/login
+// @desc   返回token jwt passport
+// @access public
+router.post('/login', (req, res) => {
+    const {email,password} = req.body;
+    // 查询数据库
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(404).json('用户不存在!');
+        }
+        // 密码匹配
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const rule = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                };
+                //登录后返回token
+                // jwt.sign("规则","加密名字","过期时间","箭头函数")
+                jwt.sign(rule, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    });
+                });
+                // res.json({msg:"success"});
+            } else {
+                return res.status(400).json('密码错误!');
+            }
+        });
+    });
+});
 
 module.exports = router;
