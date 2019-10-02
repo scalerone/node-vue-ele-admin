87bce4288de7c5c34b502b8652b5338c684d1545
scalerone <676119236@qq.com>
2019-10-02 11:53:38 +0800

    express简单搭建服务器

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
