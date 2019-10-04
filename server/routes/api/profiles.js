1fbfbf81cb65b17102452a5db6afc17bf5c7ceb8
scalerone <676119236@qq.com>
2019-10-04 11:49:50 +0800

    完成编辑和删除账单信息接口

--- server/routes/api/profiles.js
+++ server/routes/api/profiles.js
@@ -73,4 +73,44 @@ router.get(
     }
 );
 
+// @route  POST api/profiles/edit
+// @desc   编辑信息接口
+// @access Private
+router.post(
+    '/edit/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const profileFields = {};
+
+        if (req.body.type) profileFields.type = req.body.type;
+        if (req.body.describe) profileFields.describe = req.body.describe;
+        if (req.body.income) profileFields.income = req.body.income;
+        if (req.body.expend) profileFields.expend = req.body.expend;
+        if (req.body.cash) profileFields.cash = req.body.cash;
+        if (req.body.remark) profileFields.remark = req.body.remark;
+
+        Profile.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: profileFields },
+            { new: true }
+        ).then(profile => res.json(profile));
+    }
+);
+
+// @route  POST api/profiles/delete/:id
+// @desc   删除信息接口
+// @access Private
+router.delete(
+    '/delete/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOneAndRemove({ _id: req.params.id })
+            .then(profile => {
+                profile.save().then(profile => res.json(profile));
+            })
+            .catch(err => res.status(404).json('删除失败!'));
+    }
+);
+
+
 module.exports = router;
