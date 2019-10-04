017c9cd3e68c32c604e0d8294fa7061390df1395
scalerone <676119236@qq.com>
2019-10-04 11:33:01 +0800

    完成账单添加、获取所有账单和单个账单接口

--- server/routes/api/profiles.js
+++ server/routes/api/profiles.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const Profile = require('../../models/Profile');
 
 // @route  GET api/profiles/test
@@ -10,5 +10,67 @@ const Profile = require('../../models/Profile');
 router.get('/test', (req, res) => {
   res.json({ msg: 'profile works' });
 });
+
+// @route  POST api/profiles/add
+// @desc   创建账单添加接口
+// @access Private
+router.post(
+    '/add',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const profileFields = {};
+
+        if (req.body.type) profileFields.type = req.body.type;
+        if (req.body.describe) profileFields.describe = req.body.describe;
+        if (req.body.income) profileFields.income = req.body.income;
+        if (req.body.expend) profileFields.expend = req.body.expend;
+        if (req.body.cash) profileFields.cash = req.body.cash;
+        if (req.body.remark) profileFields.remark = req.body.remark;
+
+        new Profile(profileFields).save().then(profile => {
+            res.json(profile);
+        });
+    }
+);
+
+// @route  GET api/profiles
+// @desc   获取所有信息
+// @access Private
+
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.find()
+            .then(profile => {
+                if (!profile) {
+                    return res.status(404).json('没有任何内容');
+                }
+
+                res.json(profile);
+            })
+            .catch(err => res.status(404).json(err));
+    }
+);
+
+// @route  GET api/profiles/:id
+// @desc   获取单个信息
+// @access Private
+
+router.get(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOne({ _id: req.params.id })
+            .then(profile => {
+                if (!profile) {
+                    return res.status(404).json('没有任何内容');
+                }
+
+                res.json(profile);
+            })
+            .catch(err => res.status(404).json(err));
+    }
+);
 
 module.exports = router;
