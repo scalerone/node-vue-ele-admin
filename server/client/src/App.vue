5671b7cf554e1279c318258fabafcbadf6f60a93
scalerone <676119236@qq.com>
2019-10-04 21:00:48 +0800

    精简初始化的工程代码，引入重置样式

--- server/client/src/App.vue
+++ server/client/src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+  html,body,
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +13,5 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ server/client/src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+            hello vuejs
+    </div>
+</template>
+<script>
+
+</script>
+
+
+<style>
+
+</style>
